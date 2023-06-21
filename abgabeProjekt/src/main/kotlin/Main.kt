8e8db3535f679883dc717b96a71751dbadffa371
Johannes0603 <117707134+Johannes0603@users.noreply.github.com>
2023-06-21 16:39:39 +0200

    Add files via upload

--- abgabeProjekt/src/main/kotlin/Main.kt
+++ abgabeProjekt/src/main/kotlin/Main.kt
@@ -4,26 +4,21 @@ import kotlin.random.nextInt
 fun main(args: Array<String>) {
 
 
+    val eigeneEinheiten = mutableListOf<Held>()
+    eigeneEinheiten.add(Magier("testbernd", 20, 50))
+    eigeneEinheiten.add(Held("Peter", 100, mana = 20))
+    eigeneEinheiten.add(Tank("tankbernd",200, 10))
 
-    val gut1 = Magier("testbernd", 20, 500)
-    val gut2 = Held("Peter", 100, mana = 20)
-    val gut3 = Tank("tankbernd",200, 10)
-    val eigeneEinheiten = listOf(gut1, gut2, gut3)
     val bös1 = Lichtbringer("dolf", 1000, 10001)
    // val bös2 = beschworener("grässlicher", 50)
     val gegner = mutableListOf<beschworener>()
     var round = 1
-    // loop
-    while (gut1.lebendig() or gut2.lebendig() or gut3.lebendig()
-        && bös1.lebendig()) {
+    // loop(gut1.lebendig() or gut2.lebendig() or gut3.lebendig()
+
+    while (eigeneEinheiten.any { it.lebendig() } && bös1.lebendig()) {
         println("runde: $round")
         println("$eigeneEinheiten treffen auf $bös1 $gegner\n")
         round++
-
-    bös1.aktion(eigeneEinheiten)
-        if (eigeneEinheiten.all { it.tot() }){
-        println("Das böse siegt")
-        break }
 
         eigeneEinheiten.forEach() { Held ->
         println("${Held.name} ist an der Reihe.Was möchtest du tun?:")
@@ -42,6 +37,18 @@ fun main(args: Array<String>) {
                 Held.provoziert = true
             }
             }else -> println("ungültige Auswahl") } }
+        bös1.aktion(eigeneEinheiten)
+
+        if (eigeneEinheiten.all {it.tot()}){
+            println("Gut wurde besiegt")
+            break
+        }
+        if (!bös1.lebendig() && gegner.all {it.tot()})
+            println("das böse wurde besiegt")
+        break}
+       /* if (eigeneEinheiten.all { it.tot() }){
+            println("Das böse siegt")
+            break }
     if (bös1.tot()) {
         println("das böse wurde besiegt")
         break }
@@ -50,5 +57,5 @@ fun main(args: Array<String>) {
         println("${bös1.name} wurde besiegt")
     }else if (!bös1.lebendig()){
         println("$eigeneEinheiten wurde besiegt")
-    break}
-}}
+    break}*/
+}
